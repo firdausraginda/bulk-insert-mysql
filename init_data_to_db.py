8b784f62de0efce26f2e1f7d483544647fd9510d
firdausraginda <ragindafirdaus01@gmail.com>
2024-02-24 17:22:02 +0700

    adjust code to able execute per query command

--- init_data_to_db.py
+++ init_data_to_db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 db_conn = mysql.connector.Connect(
    host="localhost", 
    user="dbeaver", 
@@ -9,15 +10,22 @@ db_conn = mysql.connector.Connect(
 
 db_cursor = db_conn.cursor()
 
-with open("./127-0-0-1dummy_baltini20231206-12-23.sql", encoding="utf-8") as f:
-    commands = f.read().split(';')
+path_to_file = "./127-0-0-1dummy_baltini20231206-12-23.sql"
+with open(path_to_file, encoding="utf-8") as f:
+    commands = f.read().split('\n\n')
 
-for command in commands:
+for command in commands:    
     try:
-        db_cursor.execute(command)
+        if not command.strip().startswith("#"):
+            temp_sub_command = ""
+            for per_line_command in command.strip().split("\n"):
+                temp_sub_command += per_line_command
+                if per_line_command.endswith(";"):
+                    db_cursor.execute(temp_sub_command)
+                    temp_sub_command = ""
+            
     except Exception as e:
-        continue
-    else:
         print(command)
+        raise e
 
 db_conn.close()
