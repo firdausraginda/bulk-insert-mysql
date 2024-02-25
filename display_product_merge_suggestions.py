9c61cf4fe4e815f6e67eaab44f6aa83a57c21493
firdausraginda <ragindafirdaus01@gmail.com>
2024-02-25 22:40:13 +0700

    remove pandas

--- display_product_merge_suggestions.py
+++ display_product_merge_suggestions.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import pandas as pd
 
 
 db_conn = mysql.connector.Connect(
@@ -9,16 +8,25 @@ db_conn = mysql.connector.Connect(
    password="dbeaver",
    database="public")
 
+db_cursor = db_conn.cursor()
+
 query = """
 SELECT
 		pd.title AS group_title,
 		count(pdl.product_id) AS product_count,
 		pd.updated_at
-	FROM public.product_duplicate_lists_dummy pdl
-	LEFT JOIN public.product_duplicates_dummy pd ON
+	FROM public.product_duplicate_lists pdl
+	LEFT JOIN public.product_duplicates pd ON
 		pdl.product_duplicate_id = pd.id
 	GROUP BY 1,3;
 """
 
-df = pd.read_sql(query, db_conn)
-print(df.head())
+try:
+    db_cursor.execute(query)
+    query_result = db_cursor.fetchall()
+    for item in query_result:
+        print(item)
+except Exception as e:
+    raise e
+
+db_conn.close()
